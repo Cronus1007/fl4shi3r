15adbbcd75ab0beecf85591aee6d607ea40f5ebd
PrerakMathur20 <mathur.prerak@gmail.com>
2021-05-26 23:55:14 +0530

    message

--- assets/js/imageslider.js
+++ assets/js/imageslider.js
@@ -1,92 +1,93 @@
- var imageSlides =
-                      document.getElementsByClassName("imageSlides");
-                    var circles = document.getElementsByClassName("circle");
-                    var leftArrow = document.getElementById("leftArrow");
-                    var rightArrow = document.getElementById("rightArrow");
-                    var counter = 0;
+  // IMAGE SLIDES & CIRCLES ARRAYS, & COUNTER
+  var imageSlides =
+  document.getElementsByClassName("imageSlides");
+var circles = document.getElementsByClassName("circle");
+var leftArrow = document.getElementById("leftArrow");
+var rightArrow = document.getElementById("rightArrow");
+var counter = 0;
 
-                    // HIDE ALL IMAGES FUNCTION
-                    function hideImages() {
-                      for (var i = 0; i < imageSlides.length; i++) {
-                        imageSlides[i].classList.remove("visible");
-                      }
-                    }
+// HIDE ALL IMAGES FUNCTION
+function hideImages() {
+  for (var i = 0; i < imageSlides.length; i++) {
+    imageSlides[i].classList.remove("visible");
+  }
+}
 
-                    // REMOVE ALL DOTS FUNCTION
-                    function removeDots() {
-                      for (var i = 0; i < imageSlides.length; i++) {
-                        circles[i].classList.remove("dot");
-                      }
-                    }
+// REMOVE ALL DOTS FUNCTION
+function removeDots() {
+  for (var i = 0; i < imageSlides.length; i++) {
+    circles[i].classList.remove("dot");
+  }
+}
 
-                    // SINGLE IMAGE LOOP/CIRCLES FUNCTION
-                    function imageLoop() {
-                      var currentImage = imageSlides[counter];
-                      var currentDot = circles[counter];
-                      currentImage.classList.add("visible");
-                      removeDots();
-                      currentDot.classList.add("dot");
-                      counter++;
-                    }
+// SINGLE IMAGE LOOP/CIRCLES FUNCTION
+function imageLoop() {
+  var currentImage = imageSlides[counter];
+  var currentDot = circles[counter];
+  currentImage.classList.add("visible");
+  removeDots();
+  currentDot.classList.add("dot");
+  counter++;
+}
 
-                    // LEFT & RIGHT ARROW FUNCTION & CLICK EVENT LISTENERS
-                    function arrowClick(e) {
-                      var target = e.target;
-                      if (target == leftArrow) {
-                        clearInterval(imageSlideshowInterval);
-                        hideImages();
-                        removeDots();
-                        if (counter == 1) {
-                          counter = imageSlides.length - 1;
-                          imageLoop();
-                          imageSlideshowInterval = setInterval(
-                            slideshow,
-                            10000
-                          );
-                        } else {
-                          counter--;
-                          counter--;
-                          imageLoop();
-                          imageSlideshowInterval = setInterval(
-                            slideshow,
-                            10000
-                          );
-                        }
-                      } else if (target == rightArrow) {
-                        clearInterval(imageSlideshowInterval);
-                        hideImages();
-                        removeDots();
-                        if (counter == imageSlides.length) {
-                          counter = 0;
-                          imageLoop();
-                          imageSlideshowInterval = setInterval(
-                            slideshow,
-                            10000
-                          );
-                        } else {
-                          imageLoop();
-                          imageSlideshowInterval = setInterval(
-                            slideshow,
-                            10000
-                          );
-                        }
-                      }
-                    }
+// LEFT & RIGHT ARROW FUNCTION & CLICK EVENT LISTENERS
+function arrowClick(e) {
+  var target = e.target;
+  if (target == leftArrow) {
+    clearInterval(imageSlideshowInterval);
+    hideImages();
+    removeDots();
+    if (counter == 1) {
+      counter = imageSlides.length - 1;
+      imageLoop();
+      imageSlideshowInterval = setInterval(
+        slideshow,
+        10000
+      );
+    } else {
+      counter--;
+      counter--;
+      imageLoop();
+      imageSlideshowInterval = setInterval(
+        slideshow,
+        10000
+      );
+    }
+  } else if (target == rightArrow) {
+    clearInterval(imageSlideshowInterval);
+    hideImages();
+    removeDots();
+    if (counter == imageSlides.length) {
+      counter = 0;
+      imageLoop();
+      imageSlideshowInterval = setInterval(
+        slideshow,
+        10000
+      );
+    } else {
+      imageLoop();
+      imageSlideshowInterval = setInterval(
+        slideshow,
+        10000
+      );
+    }
+  }
+}
 
-                    leftArrow.addEventListener("click", arrowClick);
-                    rightArrow.addEventListener("click", arrowClick);
+leftArrow.addEventListener("click", arrowClick);
+rightArrow.addEventListener("click", arrowClick);
 
-                    // IMAGE SLIDE FUNCTION
-                    function slideshow() {
-                      if (counter < imageSlides.length) {
-                        imageLoop();
-                      } else {
-                        counter = 0;
-                        hideImages();
-                        imageLoop();
-                      }
-                    }
+// IMAGE SLIDE FUNCTION
+function slideshow() {
+  if (counter < imageSlides.length) {
+    imageLoop();
+  } else {
+    counter = 0;
+    hideImages();
+    imageLoop();
+  }
+}
 
-                    // SHOW FIRST IMAGE, & THEN SET & CALL SLIDE INTERVAL
-                    setTimeout(slideshow, 1000);
-                    var imageSlideshowInterval = setInterval(slideshow, 2000);
+// SHOW FIRST IMAGE, & THEN SET & CALL SLIDE INTERVAL
+setTimeout(slideshow, 1000);
+var imageSlideshowInterval = setInterval(slideshow, 2000);
